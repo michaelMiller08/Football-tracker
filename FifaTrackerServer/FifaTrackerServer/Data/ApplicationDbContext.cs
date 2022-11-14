7317b9ef9a17a836c39fe8c5cdb856583e92c2e0
michaelMiller08 <michealkeithmiller@gmail.com>
2022-11-14 17:20:06 +0000

    backend setup
    backend was setup with basic model

---
+++ FifaTrackerServer/FifaTrackerServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FifaTrackerServer.Models;
+
+namespace FifaTrackerServer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Match> Matches { get; set; }
+    }
+}
---
+++ FifaTrackerServer/FifaTrackerServer/Models/Match.cs
@@ -0,0 +1,13 @@
+﻿namespace FifaTrackerServer.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public string Creator { get; set; }
+        public string Opponent { get; set; }
+        public int CreatorScore { get; set; }
+        public int OpponentScore { get; set; }
+    }
+}
